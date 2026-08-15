9d35b529b5c287f66d1dd9c9aa9299ae409c1f23
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-08-16 00:04:00 +0530

    Added main page and linked it to start button.

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
+"use client"
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  let router = useRouter();
   return (
     <>
       <div className="w-screen h-screen flex p-5 gap-10 flex-col items-center justify-center">
@@ -14,7 +18,10 @@ export default function Home() {
           className="drop-shadow-[0_0_15px_rgba(56,189,248,0.5)]"
           />
         </div>
-        <button className="px-8 py-3 bg-[#06B6D4] border-2 border-[#06B6D4] text-[#030712] font-mono tracking-widest rounded shadow-[0_0_15px_rgba(6,182,212,0.3)] hover:bg-[#06B6D4] hover:text-[#030712] hover:shadow-[0_0_30px_rgba(6,182,212,0.6)] hover:cursor-pointer transition-all duration-300 animate-fade-in">
+        <button 
+        className="px-8 py-3 bg-[#06B6D4] border-2 border-[#06B6D4] text-[#030712] font-mono tracking-widest rounded shadow-[0_0_15px_rgba(6,182,212,0.3)] hover:bg-[#06B6D4] hover:text-[#030712] hover:shadow-[0_0_30px_rgba(6,182,212,0.6)] hover:cursor-pointer transition-all duration-300 animate-fade-in"
+        onClick={() => router.push("/main")}
+        >
           [ START_TRACKING ]
         </button>
       </div>
